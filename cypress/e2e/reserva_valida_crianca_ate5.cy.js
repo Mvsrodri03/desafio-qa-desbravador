bbe202ee676c242e40b10664e36d47d309d58120
Mvsrodri03 <mvsrodri03@gmail.com>
2025-05-24 22:22:31 -0300

    Update reserva_valida_crianca_ate5.cy.js

--- cypress/e2e/reserva_valida_crianca_ate5.cy.js
+++ cypress/e2e/reserva_valida_crianca_ate5.cy.js
@@ -2,6 +2,10 @@ import '../support/commands';
 
 describe('Reserva completa - 2 adultos e 1 criança até 5 anos', () => {
 
+    beforeEach(() => {
+      cy.wait(1000); // Espera 500ms antes de cada teste
+    });
+
     it('Deve realizar uma reserva com sucesso', () => {
       /*
       // 1. Acessa o site
@@ -37,7 +41,47 @@ describe('Reserva completa - 2 adultos e 1 criança até 5 anos', () => {
         .first()
         .click()
 
-      cy.contains('button', 'Continuar').click()
+      cy.get('button.btn-secondary').contains('Continuar').click()
+
+      cy.wait(5000)
+
+      // Abrir o modal de hóspedes
+      cy.get('.sc-dYwfDW').contains('button', 'Hóspedes').click();
+
+     // Buscar todos os blocos de input dentro do modal
+    cy.get('.modal-body .mb-3').then((campos) => {
+      // Primeiro hóspede - Adulto 1
+      cy.wrap(campos[0]).find('input').type('DESBRAVADOR SOFTWARE');
+      cy.wrap(campos[1]).find('input').type('desbravador@email.com');
+
+      // Segundo hóspede - Adulto 2
+      cy.wrap(campos[2]).find('input').type('Maria');
+      cy.wrap(campos[3]).find('input').type('maria@email.com');
+
+      // Terceiro hóspede - Criança
+      cy.wrap(campos[4]).find('input').type('Pedro');
+      cy.wrap(campos[5]).find('input').type('pedro@email.com');
+    });
+
+      // Clicar em Salvar
+      cy.get('.modal-body').contains('button', 'Salvar').click();
+
+      // Clicar em Continuar
+      cy.get('button.btn-secondary').contains('Continuar').click()
+
+      // Dados pessoais
+      cy.get('input[name="email"]').type('desbravador@email.com');
+      cy.get('input[name="firstName"]').type('DESBRAVADOR');
+      cy.get('input[name="lastName"]').type('SOFTWARE');
+
+      
+
+      // Dados do cartão
+      cy.get('input[name="number"]').type('4000000000000044');
+      cy.get('input[name="name"]').type('DESBRAVADOR SOFTWARE');
+      cy.get('input[name="expiry"]').type('1225');
+      cy.get('input[name="cvc"]').type('123');
+
 
     });
   
